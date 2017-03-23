477ce415c80abd1212dd10e5a9197c8c6aad6b3f
emersonk <bigwhiteskier720@hotmail.com>
2017-03-23 11:11:07 -0700

    Playing around with training

--- makeBrain.m
+++ makeBrain.m
@@ -2,29 +2,25 @@ function net = makeBrain();
     [trx,trt,tsx,tst,valx,valt] = brainPreprocessImg; 
 
     %Create network:
-    hiddenLayerSize = 20;
-    net = patternnet(hiddenLayerSize);
+      hiddenLayerSize = 15;
+      net = patternnet(hiddenLayerSize);
 
+    
 %     %Train network
 %     net.LW{2,1} = net.LW{2,1}*0.01;
 %     net.b{2} = net.b{2}*0.01;
 %     net.performFcn = 'sse';         
-%     net.trainParam.goal = 0.1;    
-%     net.trainParam.show = 20;      
-%     net.trainParam.epochs = 5000;  
-%     net.trainParam.mc = 0.95;      
+    net.trainParam.goal = 0.1;    
+    net.trainParam.show = 20;      
+    net.trainParam.epochs = 5000;  
+    net.trainParam.mc = 0.95;      
+    net.trainFcn = 'trainrp';   
     [net,tr] = train(net, trx, trt);
 
     outputs = net(trx);
     errors = gsubtract(trt,outputs);
     performance = perform(net,trt,outputs);
+    
+    figure, plotconfusion(trt,outputs)
 
-    %figure, plotconfusion(trt,outputs)
-
-    %test it:
-%     img = imread('truetest.png');
-%     img = im2double(img);
-%     img = imresize(img,[20 20]);
-%     imgv = img(:);
-%     x = net(imgv);
 end
